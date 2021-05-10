a4458f849abb3781952356724b3cff74ddb12854
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-05-11 01:51:46 +0200

    added useD3 hook

--- src/components/Translator.tsx
+++ src/components/Translator.tsx
@@ -1,18 +1,38 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import useD3 from '../hooks/useD3';
 
 type TranslatorProps = {
   number: number;
 }
 
 const Translator = ({ number }: TranslatorProps) => {
+  const ref = useD3((svg) => {
+    const height: number = 50;
+    const width: number = 50;
+
+  }, [[number]]);
+  
   return (
     <div>
       <div>Number to translate : { number }</div>
       <div>
         <div>Translation</div>
         <div>
-
+          <svg 
+            ref={ref}
+            style={{
+              height: 150,
+              width: 150,
+              background: 'red'
+            }}
+          >
+            <g className="g">
+              <g className="vline"></g>
+              <g className="hline-1"></g>
+              <g className="hline-2"></g>
+            </g>
+          </svg>
         </div>
       </div>
     </div>
---
+++ src/hooks/useD3.tsx
@@ -0,0 +1,18 @@
+import React, { createRef, RefObject, useEffect, useRef } from 'react';
+import * as d3 from 'd3';
+import { Selection } from 'd3';
+
+export type RenderFnType = (svg: SVGSVGElement | null | Selection<SVGSVGElement | null, unknown, null, undefined>) => void;
+
+const useD3 = (renderFn: RenderFnType, dependencies: [number[] | null]) => {
+  const ref = useRef<SVGSVGElement | null>(null);
+
+  React.useEffect(() => {
+    renderFn(d3.select(ref.current));
+    return () => {};
+  }, dependencies);
+  
+  return ref;
+}
+
+export default useD3;
