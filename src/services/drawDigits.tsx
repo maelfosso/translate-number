5e7c7d170b42e8867c639aee7ef3e8fad6c38abc
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-05-11 11:50:43 +0200

    drawing digit 5

--- src/services/drawDigits.tsx
+++ src/services/drawDigits.tsx
@@ -22,6 +22,10 @@ export const drawDigits = (svg: Selection<SVGGElement, unknown, null, undefined>
         drawThree(g, index);
 
         break;
+      case 4:
+        drawFour(g, index);
+
+        break;
       default:
         break;
     }
@@ -85,7 +89,6 @@ const drawTwo = (svg: Selection<SVGGElement, unknown, null, undefined>, position
   }
 }
 
-
 const drawThree = (svg: Selection<SVGGElement, unknown, null, undefined>, position: number) => {
   switch (position) {
     case 0:
@@ -126,6 +129,53 @@ const drawThree = (svg: Selection<SVGGElement, unknown, null, undefined>, positi
         .attr('y1', yAxis(150))
         .attr('x2', xAxis(50))
         .attr('y2', yAxis(100))
+      
+      break;
+    default:
+      break;
+  }
+}
+
+const drawFour = (svg: Selection<SVGGElement, unknown, null, undefined>, position: number) => {
+  switch (position) {
+    case 0:
+      svg
+        .append('line')
+        .attr('class', `line two position-${position}`)
+        .attr('x1', xAxis(75))
+        .attr('y1', yAxis(50))
+        .attr('x2', xAxis(100))
+        .attr('y2', yAxis(0))
+      
+      break;
+    case 1:
+      svg
+        .append('line')
+        .attr('class', `line two position-${position}`)
+        .attr('x1', xAxis(75))
+        .attr('y1', yAxis(50))
+        .attr('x2', xAxis(50))
+        .attr('y2', yAxis(0))
+      
+      break;
+    case 2:
+      svg
+        .append('line')
+        .attr('class', `line two position-${position}`)
+        .attr('x1', xAxis(75))
+        .attr('y1', yAxis(100))
+        .attr('x2', xAxis(100))
+        .attr('y2', yAxis(150))
+      
+      break;
+    case 3:
+      svg
+        .append('line')
+        .attr('class', `line two position-${position}`)
+        .attr('x1', xAxis(75))
+        .attr('y1', yAxis(100))
+        .attr('x2', xAxis(50))
+        .attr('y2', yAxis(150))
       
       break;
     default:
